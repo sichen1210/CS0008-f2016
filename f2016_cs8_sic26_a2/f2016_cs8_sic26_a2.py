a1f780c4ad872fc0b98e667b33689cb7585db530
nitrosx (Max Novelli) <man8@pitt.edu>
2016-11-27 23:50:52 -0500

    fixes and commetns on assignment #2

--- f2016_cs8_sic26_a2/f2016_cs8_sic26_a2.py
+++ f2016_cs8_sic26_a2/f2016_cs8_sic26_a2.py
@@ -5,34 +5,39 @@
 #assignment related to file object and loop
 #chapter4 - chapter6
 
-#two fuctions needed to be defined
 
-#define input function function:
+#
+# MN: please respect indentation with comments too.
+#
 
+# two fuctions needed to be defined
+
+# define input function function:
 def processFile():
     PTN = int(line)
+    # MN: where is the rest of the function
 
 
 
-#define the output function:'
-
+# define the output function:'
 def printkv(key, value, klen=0):
     kl = max(len(key),klen)
 
-#there are three cases that the data in file object may appear:
-#string
+    # there are three cases that the data in file object may appear:
+    # string
     if isinstance(value,str):
         FS = '20s'
-#integer
+    # integer
     elif isinstance(value,int):
         FS = '10d'
-#float
+    # float
     elif isinstance(value,float):
         FS = '10.3f'
-#validation: exclude other posibilities
+    # validation: exclude other posibilities
     else:
         print("error! cannot ecxute.")
     print(format(key, str(kl) + 's'), format(value, FS))
+
 
 #main body of the program
 
@@ -42,8 +47,15 @@ def printkv(key, value, klen=0):
 PTN = 0
 PD = 0
 
+# MN: why do you hard code the file name?
+#     you should ask the user to input a file name
+#     using the python input key word
+#     Also, you need to keep in mind that the file name can change.
 #open the 1stfile we choose
 FO = open("f2016_cs8_a2.data.1.csv","r")
+
+# MN: the following block of code should be in the definition of processFile
+#
 #use loop to excute every value in file object
 for line in FO:
 #get rid of the "new line" symble
@@ -67,6 +79,8 @@ FO.close()
 PTN = 0
 PD = 0
 
+# MN: same question as above: why do you hard code the file name in the code?
+# 
 #open the 2nd file we choose
 FO = open("f2016_cs8_a2.data.2.csv","r")
 #use loop to excute every value in file object
@@ -91,7 +105,10 @@ FO.close()
 
 
 
-
+# MN: why didn't you use the quantities that you computed above?
+#     You could have saved opening the file once more!!!
+# MN: why do you use input here and not in the previous portion of the code?
+#
 #second part: chhgompute the total value
 #set initail total valus as 0:
 
@@ -103,13 +120,19 @@ TTN = 0
 File = input("f2016_cs8_a2.data.1.csv")
 
 #use while loop to test the condition
-
 while File != " " and File != "quit" and File != "q":
     fh = open(File,"r")
+    #
+    # MN: this call to the function is correct, but which quantities do you get back
+    #     given that you did not have a return statement in the function
     PD,PTN = processFile(fh)
     fh.close()
     TTD += PD
     TTN += PTN
-    Flie = input("f2016_cs8_a2.data.2.csv" )
+    #
+    # MN: here there is a type
+    #Flie = input("f2016_cs8_a2.data.2.csv" )
+    File = input("f2016_cs8_a2.data.2.csv" )
+
 printkv('total number of lines:',TTN)
 printkv('total distance:',TTD)
